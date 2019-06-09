9698ce39e1a3e04c4b2d4c6b09ba3d45606f8513
Seungheon Han <20170641@ssu.ac.kr>
2019-06-09 10:27:08 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `ㅎㅅㅎ`,
+    author: `Seungheon Han`,
+    description: `Blog.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `ㅎㅅㅎ`,
+        short_name: `ㅎㅅㅎ`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
